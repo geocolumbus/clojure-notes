181c8e47e2e713694037690350787c9516df13e2
George Campbell <geocolumbus@gmail.com>
2019-06-25 22:49:26 -0400

    Spin up repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-notes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_notes/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-notes.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-notes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
